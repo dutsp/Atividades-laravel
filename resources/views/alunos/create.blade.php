1dd47fae4edff975eec664ebe593d102d48a197d
Eduardo <eduardo@campo-real.edu.br>
2026-09-09 20:16:44 +0000

    ATV6

---
+++ resources/views/alunos/create.blade.php
@@ -0,0 +1,2 @@
+<h1>Cadastrar Aluno</h1>
+<p>Aqui será exibido o formulário de cadastro de aluno.</p>
---
+++ resources/views/alunos/edit.blade.php
@@ -0,0 +1,2 @@
+<h1>Editar Aluno</h1>
+<p>Aqui será exibido o formulário de edição de aluno.</p>
