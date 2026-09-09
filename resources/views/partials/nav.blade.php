8ec18bebf2248e270fba9aff460e01c6ad18fa3c
Eduardo <eduardo@campo-real.edu.br>
2026-09-09 20:16:55 +0000

    DESAFIO

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,6 @@
+<nav>
+    <a href="{{ url('/') }}">Início</a>
+    <a href="{{ url('/alunos-crud') }}">Alunos</a>
+    <a href="{{ url('/sobre') }}">Sobre</a>
+    <a href="{{ url('/contato') }}">Contato</a>
+</nav>
