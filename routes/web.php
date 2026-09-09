d2c6f958658a1752eff38f51682d76cbbfd1952e
Eduardo <eduardo@campo-real.edu.br>
2026-09-09 20:16:29 +0000

    ATV1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/sobre', function () {
+    return 'Esta é a página Sobre do sistema de gerenciamento de alunos.';
+});
+
+Route::get('/alunos', function () {
+    return 'Lista de alunos do sistema.';
+});
+
+Route::get('/contato', function () {
+    return 'Página de contato: contato@campo-real.edu.br';
+});
