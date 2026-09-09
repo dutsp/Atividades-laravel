3be8382db311152e5ca4a9bb6a5a5f756734bc45
Eduardo <eduardo@campo-real.edu.br>
2026-09-09 20:16:55 +0000

    ATV9

--- resources/views/alunos/index.blade.php
+++ resources/views/alunos/index.blade.php
@@ -4,4 +4,16 @@
 
 @section('content')
     <h1>Lista de Alunos</h1>
+
+    @if($alunos->isEmpty() ?? true)
+        <p>Nenhum aluno cadastrado.</p>
+    @else
+        <ul>
+            @foreach($alunos as $aluno)
+                <li>{{ $aluno->nome }} - {{ $aluno->curso }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    @include('partials.nav')
 @endsection
